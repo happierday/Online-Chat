c5db17cd4b28b5fc3dfed8b0b70ecc4948d7ff19
jian <jianchengw26@gmail.com>
2017-08-01 11:53:08 -0400

    add background and message box

---
+++ public/vendor/js/displayMessage.js
@@ -0,0 +1,12 @@
+function displayMessage(array){
+    let user = array[0].slice(25,array[0].length);
+    let message = document.createElement("div");
+    message.appendChild(document.createTextNode(user + " :  " +array[1]));
+    if(user.toUpperCase() === sessionStorage.getItem(0).toUpperCase()){
+        message.setAttribute("style","width:50%;border-radius: 10px;padding:10px;margin:5px;background:#1ddced;color:black;float:right");
+    }else{
+        message.setAttribute("style","width:50%;border-radius: 10px;padding:10px;margin:5px;background:#7FFFD4;color:black;float:left");
+    }
+    displayText.appendChild(message);
+    displayText.appendChild(document.createElement('br'));
+}
